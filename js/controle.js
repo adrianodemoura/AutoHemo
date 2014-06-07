b7fc2b8b0362d274dec1b09b042e895da7e43765
Thiago de Souza Moura <thiagodesouzamoura@gmail.com>
2014-06-07 16:55:54 -0300

    Issue #2. resolvendo o problema da desmarcação.

--- js/controle.js
+++ js/controle.js
@@ -1,11 +1,31 @@
 $(document).ready(function()
 {
-	$(" .divData select").change(function()
+	$(".divData select").change(function()
 	{
 		var dia = $("#0RetiradadataDia").val();
 		var mes = $("#0RetiradadataMes").val();
 		var ano = $("#0RetiradadataAno").val();
 		var url = base+'controle/data:'+dia+'-'+mes+'-'+ano;
 		document.location.href = url;	
+	});
+
+	$(".rdRetirada").click(function()
+	{
+		var id 		= $(this).attr('id');
+		var vlr		= $(this).val();
+		var checked = $(this).is(':checked');
+		$(this).toggleAttr('checked');
+
+		//var checked = $(this).attr('previousValue');
+		//var radioChecked = !checked;
+		//$(this).attr('checked', radioChecked);
+		/*if (checked==true)
+		{
+			$(this).attr('checked', false);
+			//$(this).removeAttr('checked');
+		} else
+		{
+			//$(this).attr('checked', true);
+		}*/
 	});
 });
--- js/padrao.js
+++ js/padrao.js
@@ -1,1 +1,11 @@
 // padrao.js
+
+/**
+ * toggleAttr Plugin
+ */
+jQuery.fn.toggleAttr = function(attr) {
+    return this.each(function() {
+        var $this = $(this);
+        $this.attr(attr) ? $this.removeAttr(attr) : $this.attr(attr, attr);
+    });
+};
