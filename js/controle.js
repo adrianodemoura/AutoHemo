1c6afebda0a6554e2e30f9154998958f1cfb702e
Adriano Carneiro de Moura <adrianodemoura@gmail.com>
2014-06-07 22:21:06 -0300

    correção na validação de retiradas e aplicados.

--- js/controle.js
+++ js/controle.js
@@ -1,5 +1,7 @@
 $(document).ready(function()
 {
+	setTotalRetiradas();
+	setTotalAplicacoes();
 	$(".divData select").change(function()
 	{
 		var dia = $("#0RetiradadataDia").val();
@@ -15,6 +17,8 @@ $(document).ready(function()
 		var vlr		= $(this).val();
 		var checked = $(this).is(':checked');
 		$(this).toggleAttr('checked');
+		setTotalRetiradas();
+		checkSoma();
 	});
 
 	$(".rdAplicacao").click(function()
@@ -23,5 +27,53 @@ $(document).ready(function()
 		var vlr		= $(this).val();
 		var checked = $(this).is(':checked');
 		$(this).toggleAttr('checked');
+		setTotalAplicacoes();
+		if (!checkSoma())
+		{
+			$(this).toggleAttr('checked');
+			setTotalAplicacoes();
+		}
 	});
 });
+
+function setTotalRetiradas()
+{
+	var tot = 0;
+	$(".rdRetirada").each(function()
+	{
+		var checked = $(this).is(':checked');
+		if (checked)
+		{
+			tot += parseFloat($(this).val());
+		}
+	});
+	$("#totRetirada").html(tot);
+}
+
+function setTotalAplicacoes()
+{
+	var tot = 0;
+	$(".rdAplicacao").each(function()
+	{
+		var checked = $(this).is(':checked');
+		if (checked)
+		{
+			tot += parseFloat($(this).val());
+		}
+	});
+	$("#totAplicado").html(tot);
+}
+
+function checkSoma()
+{
+	var totRetirada = $("#totRetirada").text();
+	var totAplicado = $("#totAplicado").text();
+	if (totAplicado>totRetirada)
+	{
+		alert("O total aplicado não pode ser maior do que o retirado !!!");
+		//$("#btSalvar").attr("disabled","disabled");
+		return false;
+	}
+	//$("#btSalvar").removeAttr("disabled");
+	return true;
+}
