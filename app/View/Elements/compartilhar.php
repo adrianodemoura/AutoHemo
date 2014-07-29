8c23909ec690856077c6fe2a3ac1daa6a2d882ba
Adriano Carneiro de Moura <adrianodemoura@gmail.com>
2014-07-29 13:05:14 -0300

    incluindo botoes de rede social.

---
+++ app/View/Elements/compartilhar.php
@@ -0,0 +1,20 @@
+<div class='btsRedeSocial'>
+<a href="http://twitter.com/share" 
+    class="bt_twitter twitter-share-button" 
+    style="<?= $Site->base ?>"
+    data-url="<?= $Site->base ?>" 
+    data-text="Gostei de" 
+    data-count="horizontal" 
+    data-via="talesam" 
+    data-lang="pt">twitter
+</a>
+
+<iframe src="http://www.facebook.com/plugins/like.php?href=<?= $Site->base ?>
+&layout=button_count&show_faces=false&width=80&action=like&font=lucida+grande&colorscheme=light" 
+allowtransparency="true" 
+style="width: 90px; height: 20px;" 
+frameborder="0" scrolling="no"></iframe>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<g:plusone size="medium"></g:plusone>
+
+</div>
